4fe3f323675491ade7f02376b82db4beb5c0d233
Micah Monfrini <ffrock905@gmail.com>
2019-01-26 16:21:48 -0600

    complete getJokes function

--- app.js
+++ app.js
@@ -4,6 +4,32 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 // FUNCTIONS
 
-function getJokes() {
-  
+function getJokes(e) {
+  const number = document.getElementById('number').value;
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      let output = '';
+
+      if(response.type === 'success') {
+        response.value.forEach((item) => {
+          output += `<li>${item.joke}</li>`;
+        });
+      } else {
+        output += `<li>Something went wrong.</li>`
+      }
+
+      document.querySelector('.jokes').innerHTML = output;
+
+      console.log(output)
+    }
+  }
+
+  xhr.send();
+
+  e.preventDefault();
 }
